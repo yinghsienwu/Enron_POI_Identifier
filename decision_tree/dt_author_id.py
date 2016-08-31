77643a9f6cb574f1f8bb250d609b390fd1aab2db
Amy Wu <acagaga@gmail.com>
2016-08-31 13:06:18 -0400

    new features

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,11 +20,17 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+print "number of features:\t",len(features_train[0])
 
 #########################################################
 ### your code goes here ###
-
+from sklearn import tree
+clf=tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
+import sklearn
+accuracy=sklearn.metrics.accuracy_score(pred,labels_test)
+print accuracy
 
 #########################################################
 
